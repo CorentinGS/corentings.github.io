6ce8010c4753b1d0713f94504f78eaed4d9adadb
CorentinGS <c.giaufersaubert@outlook.com>
2022-08-13 21:58:28 +0200

    ✨ Add skills

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,15 +1,31 @@
 <template>
-<section class="bg-base-100"
-         >
-
-
-</section>
+  <section class="bg-base-100">
+    <div class="container mx-auto px-6 py-8">
+      <div class="items-center lg:flex">
+        <div class="lg:w-1/3">
+          <h2 class="text-3xl font-bold">My Skills</h2>
+        </div>
+        <div class="mt-8 lg:mt-0 lg:w-2/3">
+          <div class="grid grid-cols-4 gap-4">
+            <Icon-logos-gopher class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-java class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-python class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-c class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-docker-icon class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-rust class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-nuxt-icon class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-tailwindcss-icon class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-rabbitmq-icon class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-git-icon class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-void class="h-16 w-16 md:h-20 md:w-20" />
+            <Icon-logos-postgresql class="h-16 w-16 md:h-20 md:w-20" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
