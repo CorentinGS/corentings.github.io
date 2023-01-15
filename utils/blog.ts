2cf4445dd387b2145fc7402fac4d305893d9e618
CorentinGS <c.giaufersaubert@outlook.com>
2023-01-15 19:34:58 +0100

    Update

--- utils/blog.ts
+++ utils/blog.ts
@@ -41,20 +41,7 @@ export class BlogPosts {
             timeToRead: "5 min",
             author: cgs,
             dark_background: true,
-        },
-        {
-            title: "My first post",
-            description: "Let's start with a simple post",
-            image:
-                "https://images.unsplash.com/photo-1503104538136-7491acef4d5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-            date: "Aug. 16, 2022",
-            link: "/blog/first",
-            category: "Life",
-            timeToRead: "5 min",
-            author: cgs,
-            dark_background: true,
         }
-
     ];
 
     static get blogPosts(): BlogPostList {
