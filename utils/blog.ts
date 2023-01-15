903e902ce4ace3ed620cccd5fb582e9a504aa752
CorentinGS <c.giaufersaubert@outlook.com>
2023-01-15 17:41:54 +0100

    up

--- utils/blog.ts
+++ utils/blog.ts
@@ -10,6 +10,17 @@ const cgs: BlogAuthor = {
 export class BlogPosts {
     private static readonly _blogPosts: BlogPostList = [
         {
+            title: "Merge Sort using Goroutines",
+            description: "This is a simple example of how to use goroutines to implement a merge sort algorithm.",
+            image: "https://images.unsplash.com/photo-1465251964418-7035fd8f889a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1491&q=80",
+            date: "Jan. 15, 2023",
+            link: "/blog/mergesort-parallel",
+            category: "Programming",    
+            timeToRead: "6 min",
+            author: cgs,
+            dark_background: true,
+        },   
+        {
             title: "Simple Function vs Goroutines",
             description: "This is a simple example of why goroutines might be overkill for some tasks and less efficient than a simple function.",
             image: "https://images.unsplash.com/photo-1622547748225-3fc4abd2cca0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
