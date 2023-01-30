0b2e6d9e5f797686f5fb5d93a937a6c99e59d25c
CorentinGS <c.giaufersaubert@outlook.com>
2023-01-30 16:16:00 +0100

    update astro

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,13 @@
-import rss from '@astrojs/rss';
+import rss, {  pagesGlobToRssItems} from '@astrojs/rss';
 
-export const get = () => rss({
-  // `<title>` field in output xml
-  title: "Corentin GS's blog",
-  // `<description>` field in output xml
-  description: 'A young programmer journey',
-  // base URL for RSS <item> links
-  // SITE will use "site" from your project's astro.config.
-  site: import.meta.env.SITE,
-  // list of `<item>`s in output xml
-  // simple example: generate items for every md file in /src/pages
-  // see "Generating items" section for required frontmatter and advanced use cases
-  items: import.meta.glob('./**/*.mdx'),
-  // (optional) inject custom xml
-  customData: `<language>en-us</language>`,
-});
+export async function get(context) {
+  return rss({
+    title: "Corentin GS's blog",
+    description: 'A young programmer journey',
+    site: import.meta.env.SITE,
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./**/*.mdx'),
+    ),
+    customData: `<language>en-us</language>`,
+  });
+}
