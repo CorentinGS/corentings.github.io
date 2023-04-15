27dab417f0181b5580554b8680898b2299c1d786
CorentinGS <c.giaufersaubert@outlook.com>
2023-04-15 16:55:21 +0200

    🚀 update astro

--- .vercel/output/functions/render.func/dist/chunks/pages/all.0b44836f.mjs
+++ .vercel/output/functions/render.func/dist/chunks/pages/all.0b44836f.mjs
@@ -5,7 +5,7 @@ async function get(context) {
     title: "Corentin GS's blog",
     description: "A young programmer journey",
     site: "https://corentings.dev",
-    items: await pagesGlobToRssItems(/* #__PURE__ */ Object.assign({"./blog/docker-and-go.mdx": () => import('../prerender.fd51db21.mjs').then(n => n.e),"./blog/mergesort-parallel.mdx": () => import('../prerender.fd51db21.mjs').then(n => n.d),"./blog/simple-go-vs-goroutines.mdx": () => import('../prerender.fd51db21.mjs').then(n => n.c)})),
+    items: await pagesGlobToRssItems(/* #__PURE__ */ Object.assign({"./blog/docker-and-go.mdx": () => import('../prerender.c33cfb44.mjs').then(n => n.e),"./blog/mergesort-parallel.mdx": () => import('../prerender.c33cfb44.mjs').then(n => n.d),"./blog/simple-go-vs-goroutines.mdx": () => import('../prerender.c33cfb44.mjs').then(n => n.c)})),
     customData: `<language>en-us</language>`
   })
 }
