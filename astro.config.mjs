c6e6d80d2d0e8a2ea8a6b6a035c2245d31cd2070
CorentinGS <c.giaufersaubert@outlook.com>
2023-05-14 19:38:54 +0200

    🚀 New avatar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,28 @@
-import { defineConfig, sharpImageService } from "astro/config"
+import { defineConfig, sharpImageService } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind"
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx"
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
-import prefetch from "@astrojs/prefetch"
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap"
+import compress from "astro-compress";
+import compressor from "astro-compressor";
 
 // https://astro.build/config
-import compress from "astro-compress"
-import compressor from "astro-compressor"
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
-import astroI18next from "astro-i18next"
+import vercel from "@astrojs/vercel/serverless";
+import critters from "astro-critters";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
@@ -31,33 +33,32 @@ export default defineConfig({
   image: {
     service: sharpImageService()
   },
-  markdown: { shikiConfig: { theme: "dracula" }, syntaxHighlight: "shiki" },
-  integrations: [
-    prefetch(),
-    tailwind(),
-    mdx({
-      shikiConfig: { theme: "dracula" },
-      syntaxHighlight: "shiki"
-    }),
-    astroI18next(),
-    sitemap({
-      lastmod: new Date(),
-      i18n: {
-        defaultLocale: "en",
-        locales: {
-          en: "en-US",
-          fr: "fr-FR",
-          de: "de-DE"
-        }
+  markdown: {
+    shikiConfig: {
+      theme: "dracula"
+    },
+    syntaxHighlight: "shiki"
+  },
+  integrations: [prefetch(), tailwind(), mdx({
+    shikiConfig: {
+      theme: "dracula"
+    },
+    syntaxHighlight: "shiki"
+  }), astroI18next(), sitemap({
+    lastmod: new Date(),
+    i18n: {
+      defaultLocale: "en",
+      locales: {
+        en: "en-US",
+        fr: "fr-FR",
+        de: "de-DE"
       }
-    }),
-    compress({
-      path: ".vercel/output/static",
-      css: false
-    }),
-    compressor()
-  ],
+    }
+  }), critters(), compress({
+    path: ".vercel/output/static",
+    css: false
+  }), compressor()],
   output: "server",
   adapter: vercel()
   // adapter: node({    mode: "standalone"   })
-})
+});
