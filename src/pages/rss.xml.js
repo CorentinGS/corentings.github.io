06f1302facc9fe649d85a9839fb7071509230186
CorentinGS <c.giaufersaubert@outlook.com>
2023-07-27 13:48:09 +0200

    🚀 Update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ export async function get(context) {
     title: "Corentin GS's blog",
     description: "A young programmer journey",
     site: import.meta.env.SITE,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.mdx")),
+    items: await pagesGlobToRssItems(import.meta.glob("./blog/*.mdx")),
     customData: `<language>en-us</language>`
   })
 }
