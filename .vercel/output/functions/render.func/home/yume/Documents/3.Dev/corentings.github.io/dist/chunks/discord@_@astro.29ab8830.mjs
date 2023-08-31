86feaa42fb573f5ba298514c6fd7719a465eaaca
CorentinGS <c.giaufersaubert@outlook.com>
2023-08-31 12:41:43 +0200

    fix

---
+++ .vercel/output/functions/render.func/home/yume/Documents/3.Dev/corentings.github.io/dist/chunks/discord@_@astro.29ab8830.mjs
@@ -0,0 +1,9 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.7e698fde.mjs';
+import 'clsx';
+import 'html-escaper';
+
+const page = () => import('./prerender.b1f65790.mjs').then(n => n.t);
+
+export { page };
---
+++ .vercel/output/functions/render.func/home/yume/Documents/3.Dev/corentings.github.io/dist/chunks/rss.f9c20a7a.mjs
@@ -0,0 +1,9 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.7e698fde.mjs';
+import 'clsx';
+import 'html-escaper';
+
+const page = () => import('./pages/rss.xml.js.e171c224.mjs');
+
+export { page };
