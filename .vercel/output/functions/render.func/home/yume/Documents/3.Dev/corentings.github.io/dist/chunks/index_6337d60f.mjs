8437c81af229ab7b544e1c21a39b2a225e9fe951
corentings <contact@corentings.dev>
2023-11-14 11:30:40 +0100

    update

--- .vercel/output/functions/render.func/home/yume/Documents/3.Dev/corentings.github.io/dist/chunks/index_6337d60f.mjs
+++
@@ -1,9 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import './astro_a71c612b.mjs';
-import 'clsx';
-import 'html-escaper';
-
-const page = () => import('./prerender_9b2d59c3.mjs').then(n => n.k);
-
-export { page };
--- .vercel/output/functions/render.func/home/yume/Documents/3.Dev/corentings.github.io/dist/chunks/rss_065c83f2.mjs
+++
@@ -1,9 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import './astro_a71c612b.mjs';
-import 'clsx';
-import 'html-escaper';
-
-const page = () => import('./pages/rss_fee02147.mjs');
-
-export { page };
