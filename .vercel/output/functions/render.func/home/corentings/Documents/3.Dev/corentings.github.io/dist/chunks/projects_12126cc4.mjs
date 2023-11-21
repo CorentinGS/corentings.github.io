9377d981d710d3aa300a4e25677aef0a770e2e66
corentings <contact@corentings.dev>
2023-11-21 23:20:22 +0100

    Better layout

---
+++ .vercel/output/functions/render.func/home/corentings/Documents/3.Dev/corentings.github.io/dist/chunks/projects_12126cc4.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./prerender_a872f4a3.mjs').then(n => n.z);
+
+export { page };
---
+++ .vercel/output/functions/render.func/home/corentings/Documents/3.Dev/corentings.github.io/dist/chunks/rss_810c8634.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/rss_16f00152.mjs');
+
+export { page };
