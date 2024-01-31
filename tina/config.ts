ac77555e06f8205e5393638b332501294ef19293
CorentinGS <contact@corentings.dev>
2024-01-31 16:58:53 +0100

    :children_crossing: TineCMS

--- tina/config.ts
+++ tina/config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   media: {
     tina: {
       mediaRoot: '/src/assets/images',
-      publicFolder: "public",
+      publicFolder: "",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
