f3d627c7babcfffc55e1398da37dd1262162c340
CorentinGS <contact@corentings.dev>
2024-01-31 18:58:14 +0100

    :children_crossing: TinaCMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,60 +5,63 @@ import vercel from '@astrojs/vercel/serverless'
 import compress from 'astro-compress'
 import compressor from 'astro-compressor'
 import astroI18next from 'astro-i18next'
-import { defineConfig, sharpImageService, squooshImageService } from 'astro/config'
+import {defineConfig, sharpImageService, squooshImageService} from 'astro/config'
+import {imageService} from "@unpic/astro/service";
 
-import { remarkReadingTime } from './src/utils/readTime.ts'
+
+import {remarkReadingTime} from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://corentings.dev',
-	image: {
-		service: process.env.NODE_ENV === 'production' ? sharpImageService() : squooshImageService()
-	},
-	prefetch: {
-		prefetchAll: false,
-		defaultStrategy: 'hover'
-	},
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	integrations: [
-		tailwind(),
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				theme: 'material-theme-palenight',
-				wrap: true
-			},
-			drafts: true
-		}),
-		astroI18next(),
-		sitemap({
-			lastmod: new Date(),
-			i18n: {
-				defaultLocale: 'en',
-				locales: {
-					en: 'en-US',
-					fr: 'fr-FR',
-					de: 'de-DE'
-				}
-			}
-		}),
-		compress({
-			path: '.vercel/output/static',
-			CSS: false,
-			HTML: false,
-			JavaScript: false
-		}),
-		compressor()
-	],
-	output: 'server',
-	adapter: vercel({
-		functionPerRoute: false
-	})
+    site: 'https://corentings.dev',
+    image: {
+        // service: process.env.NODE_ENV === 'production' ? sharpImageService() : squooshImageService()
+        service: imageService()
+    },
+    prefetch: {
+        prefetchAll: false,
+        defaultStrategy: 'hover'
+    },
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+        drafts: true,
+        shikiConfig: {
+            theme: 'material-theme-palenight',
+            wrap: true
+        }
+    },
+    integrations: [
+        tailwind(),
+        mdx({
+            syntaxHighlight: 'shiki',
+            shikiConfig: {
+                theme: 'material-theme-palenight',
+                wrap: true
+            },
+            drafts: true
+        }),
+        astroI18next(),
+        sitemap({
+            lastmod: new Date(),
+            i18n: {
+                defaultLocale: 'en',
+                locales: {
+                    en: 'en-US',
+                    fr: 'fr-FR',
+                    de: 'de-DE'
+                }
+            }
+        }),
+        compress({
+            path: '.vercel/output/static',
+            CSS: false,
+            HTML: false,
+            JavaScript: false
+        }),
+        compressor()
+    ],
+    output: 'server',
+    adapter: vercel({
+        functionPerRoute: false
+    })
 })
