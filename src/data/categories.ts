e34990b02ec4e152e9cb942ccda7dd70f86b44c7
CorentinGS <contact@corentings.dev>
2024-01-31 17:09:50 +0100

    :children_crossing: TineCMS

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,6 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Linux',
+	'Programming',
+	'Chess',
 ] as const
