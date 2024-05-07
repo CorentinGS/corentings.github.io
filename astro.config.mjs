1f709c42b2239a30b4d8e3e2480275c14f862eab
corentings <contact@corentings.dev>
2024-05-07 17:43:23 +0200

    🐛 fix(fix): fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,48 +1,42 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import compressor from 'astro-compressor'
-import { defineConfig, sharpImageService, squooshImageService } from 'astro/config'
-
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import compressor from 'astro-compressor';
+import { defineConfig, sharpImageService, squooshImageService } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import { remarkReadingTime } from './src/utils/readTime.ts';
 
-
-import { remarkReadingTime } from './src/utils/readTime.ts'
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://corentings.dev',
-	image: {
-		service: process.env.NODE_ENV === 'production' ? sharpImageService() : squooshImageService()
-	},
-	prefetch: {
-		prefetchAll: false,
-		defaultStrategy: 'hover'
-	},
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	integrations: [
-		tailwind(),
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				theme: 'material-theme-palenight',
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap({
-			lastmod: new Date(),
-		}),
-		(await import("@playform/compress")).default(),
-		compressor()
-	],
-	output: 'hybrid',
-	adapter: cloudflare()
-})
+  site: 'https://corentings.dev',
+
+  image: {
+    service: process.env.NODE_ENV === 'production' ? sharpImageService() : squooshImageService()
+  },
+  prefetch: {
+    prefetchAll: false,
+    defaultStrategy: 'hover'
+  },
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    drafts: true,
+    shikiConfig: {
+      theme: 'material-theme-palenight',
+      wrap: true
+    }
+  },
+  integrations: [tailwind(), mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'material-theme-palenight',
+      wrap: true
+    },
+    drafts: true
+  }), sitemap({
+    lastmod: new Date()
+  }), playformInline(), (await import("@playform/compress")).default(), compressor()],
+  output: 'static',
+  // adapter: cloudflare()
+});
