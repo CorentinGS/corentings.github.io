ae8ef432e984b3ebccbdc5be079a55ad0d2bcddf
corentings <contact@corentings.dev>
2024-05-07 14:23:18 +0200

    ✨ feat: update content and new projects/experiences

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,20 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+import { link } from 'fs'
 
 export async function get(context) {
+	const posts = await getCollection('blog')
+
 	return rss({
 		title: "Corentin GS's blog",
 		description: 'A young programmer journey',
 		site: import.meta.env.SITE,
-		items: await pagesGlobToRssItems(import.meta.glob('./blog/*.mdx')),
+		items: posts.map((post) => ({
+			title: post.title,
+			description: post.description,
+			url: post.url,
+			date: post.date,
+			link: `/blog/${post.slug}`,
+		})),
 		customData: `<language>en-us</language>`
 	})
 }
