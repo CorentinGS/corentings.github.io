4add8ae3e2da31dc7528d72dcf55c533f7e10311
corentings <contact@corentings.dev>
2024-07-19 15:42:18 +0200

    🎨 style(rss): improve rss style and create autogen

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,29 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+    const blog = await getCollection('blog');
+    return rss({
+        title: "Corentin GS's blog",
+        stylesheet: '/styles.xsl',
+        description: 'A young programmer journey',
+        site: context.site,
+        items: blog.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            customData: `<language>en-us</language> <category>${post.data.category}</category> <tags>${post.data.tags}</tags>`,
+            author: "Corentin GS",
+            // Compute RSS link from post `slug`
+            // This example assumes all posts are rendered as `/blog/[slug]` routes
+            link: `/blog/${post.slug}/`,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+              }),
+        })),
+    });
+}
