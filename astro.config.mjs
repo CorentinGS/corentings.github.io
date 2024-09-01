8d86e507a74c090a026c0ba3c7499e43ecfcbe5a
Corentin Giaufer Saubert <43623834+CorentinGS@users.noreply.github.com>
2024-09-01 14:15:41 +0200

    Feat/partytown (#213)
    * ⚡️ perf: update images to use webp and have better compression
    
    * 🍱 asset: compress favicons
    
    * fix: Update heroImage path in crafting-the-echiquier-nicois-website.mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,48 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import compressor from 'astro-compressor'
-import { defineConfig, sharpImageService, squooshImageService } from 'astro/config'
-import { remarkReadingTime } from './src/utils/readTime.ts'
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import compressor from 'astro-compressor';
+import { defineConfig, sharpImageService, squooshImageService } from 'astro/config';
+import { remarkReadingTime } from './src/utils/readTime.ts';
+
+import partytown from "@astrojs/partytown";
+
+import playformCompress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://corentings.dev',
-	image: {
-		service: process.env.NODE_ENV === 'production' ? sharpImageService() : squooshImageService()
-	},
-	prefetch: {
-		prefetchAll: false,
-		defaultStrategy: 'hover'
-	},
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	integrations: [
-		tailwind(),
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				theme: 'material-theme-palenight',
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap({
-			lastmod: new Date()
-		}),
-		compressor()
-	],
-	output: 'static'
-	// adapter: cloudflare()
-})
+    site: 'https://corentings.dev',
+    image: {
+        service: process.env.NODE_ENV === 'production' ? sharpImageService() : squooshImageService()
+    },
+    prefetch: {
+        prefetchAll: false,
+        defaultStrategy: 'hover'
+    },
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+        drafts: true,
+        shikiConfig: {
+            theme: 'material-theme-palenight',
+            wrap: true
+        }
+    },
+    integrations: [partytown(), tailwind(), mdx({
+        syntaxHighlight: 'shiki',
+        shikiConfig: {
+            theme: 'material-theme-palenight',
+            wrap: true
+        },
+        drafts: true
+    }), sitemap({
+        lastmod: new Date()
+    }), (await import("@playform/compress")).default({
+		CSS: true,
+		HTML: true,
+		Image: false,
+		JavaScript: false,
+		SVG: true,
+	}), compressor()],
+    output: 'static'
+    // adapter: cloudflare()
+});
