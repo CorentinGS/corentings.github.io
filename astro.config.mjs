72dd47eac7b021f6c92ec50566c9f8589c763f50
corentings <contact@corentings.dev>
2024-09-01 12:21:03 +0000

    feat: Update Astro configuration and remove unused dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,44 +5,34 @@ import compressor from 'astro-compressor';
 import { defineConfig, sharpImageService, squooshImageService } from 'astro/config';
 import { remarkReadingTime } from './src/utils/readTime.ts';
 
-import partytown from "@astrojs/partytown";
-
-import playformCompress from '@playform/compress';
-
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://corentings.dev',
-    image: {
-        service: process.env.NODE_ENV === 'production' ? sharpImageService() : squooshImageService()
-    },
-    prefetch: {
-        prefetchAll: false,
-        defaultStrategy: 'hover'
-    },
-    markdown: {
-        remarkPlugins: [remarkReadingTime],
-        drafts: true,
-        shikiConfig: {
-            theme: 'material-theme-palenight',
-            wrap: true
-        }
-    },
-    integrations: [partytown(), tailwind(), mdx({
-        syntaxHighlight: 'shiki',
-        shikiConfig: {
-            theme: 'material-theme-palenight',
-            wrap: true
-        },
-        drafts: true
-    }), sitemap({
-        lastmod: new Date()
-    }), (await import("@playform/compress")).default({
-		CSS: true,
-		HTML: true,
-		Image: false,
-		JavaScript: false,
-		SVG: true,
+	site: 'https://corentings.dev',
+	image: {
+		service: process.env.NODE_ENV === 'production' ? sharpImageService() : squooshImageService()
+	},
+	prefetch: {
+		prefetchAll: false,
+		defaultStrategy: 'hover'
+	},
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	integrations: [tailwind(), mdx({
+		syntaxHighlight: 'shiki',
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		},
+		drafts: true
+	}), sitemap({
+		lastmod: new Date()
 	}), compressor()],
-    output: 'static'
-    // adapter: cloudflare()
+	output: 'static'
+	// adapter: cloudflare()
 });
