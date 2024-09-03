59e87d4d657db40fa6719cbc0317cdcc56ba49a0
corentings <contact@corentings.dev>
2024-09-03 09:31:10 +0000

    Update image import in ListRelatedPosts.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import compressor from 'astro-compressor';
-import { defineConfig, sharpImageService, squooshImageService } from 'astro/config';
-import { remarkReadingTime } from './src/utils/readTime.ts';
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import compressor from 'astro-compressor'
+import { defineConfig, sharpImageService, squooshImageService } from 'astro/config'
+import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
@@ -27,16 +27,21 @@ export default defineConfig({
 			wrap: true
 		}
 	},
-	integrations: [tailwind(), mdx({
-		syntaxHighlight: 'shiki',
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		},
-		drafts: true
-	}), sitemap({
-		lastmod: new Date()
-	}), compressor()],
+	integrations: [
+		tailwind(),
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				theme: 'material-theme-palenight',
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap({
+			lastmod: new Date()
+		}),
+		compressor()
+	],
 	output: 'static'
 	// adapter: cloudflare()
-});
+})
