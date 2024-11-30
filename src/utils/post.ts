aea2a0c56e58c0c3d6a6f4d2c48970426cc76439
Corentin Giaufer Saubert <43623834+CorentinGS@users.noreply.github.com>
2024-11-30 18:36:46 +0100

    Article/generator pattern (#314)
    * feat: generator-pattern
    
    * fix: hide future posts

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -8,10 +8,11 @@ export const getCategories = async () => {
 
 export const getPosts = async (max?: number) => {
 	return (await getCollection('blog'))
-		.filter((post) => !post.data.draft)
+		.filter((post) => !post.data.draft && new Date(post.data.pubDate) <= new Date())
 		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
 		.slice(0, max)
 }
+
 
 export const getTags = async () => {
 	const posts = await getCollection('blog')
